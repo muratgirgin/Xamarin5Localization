3caaceddeefe20e6a87f750a3f589217b5e000ae
Murat <muradgirgin@gmail.com>
2021-08-17 00:02:34 +0300

    Add project files.

---
+++ Xamarin5Localization/ChangeLanguagePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Threading;
+using Xamarin5Localization.Resources;
+using Xamarin.Forms;
+
+namespace Xamarin5Localization.Views
+{
+    public partial class ChangeLanguagePage : ContentPage
+    {
+        public ChangeLanguagePage()
+        {
+            InitializeComponent();
+
+
+        }
+
+        void OnUpdateLangugeClicked(object sender, System.EventArgs e)
+        {
+            if (picker.SelectedItem != null)
+            {
+                var language = CultureInfo.GetCultures(CultureTypes.NeutralCultures).ToList().First(element => element.EnglishName.Contains(picker.SelectedItem.ToString()));
+                Thread.CurrentThread.CurrentUICulture = language;
+                AppResources.Culture = language;
+                App.Current.MainPage = new NavigationPage(new MainPage());
+            }
+        }
+    }
+}
---
+++ Xamarin5Localization/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using Xamarin5Localization.Resources;
+using Xamarin5Localization.Views;
+using Xamarin.Forms;
+
+namespace Xamarin5Localization
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new NavigationPage(new ChangeLanguagePage())); 
+        }
+    }
+}
